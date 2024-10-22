4de7cc661e4d2356801c0314037fd9a5467eecbd
Ami.Raian <ami.robirai@gmail.com>
2024-10-23 01:33:34 +0600

    payment option done

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -16,7 +16,7 @@ const Payment = () => {
     // }
     return (
         <div>
-            <h3 className="text-3xl">Payment for {treatment}</h3>
+            <h3 className="text-3xl my-10">Payment for {treatment}</h3>
             <p className="text-xl">Please pay <strong>${price}</strong> for your appointment on {appointmentDate} at {slot}</p>
             <div className='w-96 my-12'>
                 <Elements stripe={stripePromise}>
